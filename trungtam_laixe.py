c09743b0125a07e11edacf7adf4148d4a8f34d57
shoptinhoc84 <142798880+shoptinhoc84@users.noreply.github.com>
2026-02-13 13:24:06 +0700

    Update trungtam_laixe.py

--- trungtam_laixe.py
+++ trungtam_laixe.py
@@ -53,7 +53,7 @@ with tab4:
         st.subheader("🛵 Hạng Xe Máy")
         st.write("Bộ đề ôn tập 250 câu hỏi luật giao thông đường bộ mới nhất.")
         # BẠN THAY LINK CỦA BẠN VÀO DÒNG DƯỚI ĐÂY
-        st.link_button("Học 250 Câu Xe Máy", "https://duong-link-250-cau-cua-ban.com")
+        st.link_button("Học 250 Câu Xe Máy", "https://daotaolaixehd.com.vn/bo-de-250-cau-ly-thuyet-thi-lai-xe-may")
         
     with col_oto:
         st.subheader("🚗 Hạng Ô Tô")
@@ -73,3 +73,4 @@ with tab6:
 
 st.markdown("---")
 st.caption("© 2026 Phương Thúy - Tận tâm và Uy tín.")
+
